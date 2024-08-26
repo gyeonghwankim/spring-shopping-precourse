0c9ae9681625765a8a7b750cf06e8210ae41eeb0
Gyeonghwan Kim <gyeonghwan.dev@gmail.com>
2024-08-26 17:16:09 +0900

    docs: 용어 정리 추가

--- src/test/java/shopping/util/ValidatorTest.java
+++ src/test/java/shopping/util/ValidatorTest.java
@@ -1,12 +1,10 @@
 package shopping.util;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import shopping.domain.ProductMsgResponse;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ValidatorTest {
 
